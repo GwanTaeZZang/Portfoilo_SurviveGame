2db936c27d5993a8498d8d5d345d0e39773f35bb
김태관 <>
2024-04-24 20:54:27 +0900

    [mod] 베기 오브젝트 json 추가

--- SurviveGame/Assets/FileIO/CreateJson/TestCreateJson.cs
+++ SurviveGame/Assets/FileIO/CreateJson/TestCreateJson.cs
@@ -27,7 +27,7 @@ public class TestCreateJson : MonoBehaviour
         job1.increaseStatus = new StatusEffect[1];
 
         StatusEffect effect = new StatusEffect();
-        effect.stringKey = "데미지 버프";
+        effect.stringKey = "?????? ????";
         effect.effectType = StatusEffectType.Damage;
         effect.amount = +10;
         job1.increaseStatus[0] = effect;
@@ -35,23 +35,23 @@ public class TestCreateJson : MonoBehaviour
         job1.decreaseStatus = new StatusEffect[2];
 
         effect = new StatusEffect();
-        effect.stringKey = "공격속도 너프";
+        effect.stringKey = "???????? ????";
         effect.effectType = StatusEffectType.AttackSpeed;
         effect.amount = -3;
         job1.decreaseStatus[0] = effect;
 
         effect = new StatusEffect();
-        effect.stringKey = "속도 너프";
+        effect.stringKey = "???? ????";
         effect.effectType = StatusEffectType.Speed;
         effect.amount = -3;
         job1.decreaseStatus[1] = effect;
 
         job1.unLockId = 10003;
 
-        job1.stringKey = "힘 바보";
+        job1.stringKey = "?? ????";
 
         job1.jobSpritePath = "Sprite/Job/Job_0";
-        job1.jobName = "힘 바보";
+        job1.jobName = "?? ????";
 
         jobArr[0] = job1;
 
@@ -62,13 +62,13 @@ public class TestCreateJson : MonoBehaviour
         job2.increaseStatus = new StatusEffect[2];
 
         effect = new StatusEffect();
-        effect.stringKey = "공격속도 버프";
+        effect.stringKey = "???????? ????";
         effect.effectType = StatusEffectType.AttackSpeed;
         effect.amount = +5;
         job2.increaseStatus[0] = effect;
 
         effect = new StatusEffect();
-        effect.stringKey = "속도 버프";
+        effect.stringKey = "???? ????";
         effect.effectType = StatusEffectType.Speed;
         effect.amount = +5;
         job2.increaseStatus[1] = effect;
@@ -76,17 +76,17 @@ public class TestCreateJson : MonoBehaviour
         job2.decreaseStatus = new StatusEffect[1];
 
         effect = new StatusEffect();
-        effect.stringKey = "데미지 너프";
+        effect.stringKey = "?????? ????";
         effect.effectType = StatusEffectType.Damage;
         effect.amount = -7;
         job2.decreaseStatus[0] = effect;
 
         job2.unLockId = 10004;
 
-        job2.stringKey = "속도 바보";
+        job2.stringKey = "???? ????";
 
         job2.jobSpritePath = "Sprite/Job/Job_1";
-        job2.jobName = "속도 바보";
+        job2.jobName = "???? ????";
 
         jobArr[1] = job2;
 
@@ -97,7 +97,7 @@ public class TestCreateJson : MonoBehaviour
 
     private void CreateWeaponJson()
     {
-        WeaponItemInfo[] weaponInfoArr = new WeaponItemInfo[2];
+        WeaponItemInfo[] weaponInfoArr = new WeaponItemInfo[3];
 
         WeaponItemInfo info = new WeaponItemInfo();
         info.Uid = 2000;
@@ -105,10 +105,10 @@ public class TestCreateJson : MonoBehaviour
         info.damage = 10;
         info.damageRate = 0.5f;
         info.attackSpeed = 1f;
-        info.attackRange = 3f;
-        info.stringKey = "삼지창";
+        info.attackRange = 2f;
+        info.stringKey = "???";
         info.weaponSpritePath = "Sprite/Weapon/Weapon_1";
-        info.weaponName = "삼지창";
+        info.weaponName = "???";
         info.weaponType = WeaponType.StingWeapon;
         weaponInfoArr[0] = info;
 
@@ -118,35 +118,49 @@ public class TestCreateJson : MonoBehaviour
         info.damage = 5;
         info.damageRate = 0.3f;
         info.attackSpeed = 1f;
-        info.attackRange = 10f;
-        info.stringKey = "엽총";
+        info.attackRange = 5f;
+        info.stringKey = "??";
         info.weaponSpritePath = "Sprite/Weapon/Weapon_3";
-        info.weaponName = "엽총";
+        info.weaponName = "??";
         info.weaponType = WeaponType.ShoootingWeapon;
         weaponInfoArr[1] = info;
+
+        info = new WeaponItemInfo();
+        info.Uid = 2002;
+        info.level = 1;
+        info.damage = 5;
+        info.damageRate = 0.3f;
+        info.attackSpeed = 1f;
+        info.attackRange = 2f;
+        info.stringKey = "?? ?";
+        info.weaponSpritePath = "Sprite/Weapon/Weapon_2";
+        info.weaponName = "?? ?";
+        info.weaponType = WeaponType.MowWeapon;
+        weaponInfoArr[2] = info;
+
 
         weaponData = new WeaponData();
         weaponData.weaponArr = weaponInfoArr;
     }
 
-    [ContextMenu("To Json Character Job Data")]  // 컴포넌트 메뉴에 아래 함수를 호출하는 To Json Data 라는 명렁어가 생성됨 
+    [ContextMenu("To Json Character Job Data")]  // ???????? ?????? ???? ?????? ???????? To Json Data ???? ???????? ?????? 
     private void SaveToJsonCharacterJobData()
     {
         if (jobFileName == "")
         {
-            Debug.Log("파일 이름 없음 ");
+            Debug.Log("???? ???? ???? ");
             return;
         }
         JsonController.WriteJson<JobData>(jobFileName, jobData);
     }
 
 
-    [ContextMenu("To Json Weapon Data")]  // 컴포넌트 메뉴에 아래 함수를 호출하는 To Json Data 라는 명렁어가 생성됨 
+    [ContextMenu("To Json Weapon Data")]  // ???????? ?????? ???? ?????? ???????? To Json Data ???? ???????? ?????? 
     private void SaveToJsonWeaponData()
     {
         if (weaponFileName == "")
         {
-            Debug.Log("파일 이름 없음 ");
+            Debug.Log("???? ???? ???? ");
             return;
         }
         JsonController.WriteJson<WeaponData>(weaponFileName, weaponData);
